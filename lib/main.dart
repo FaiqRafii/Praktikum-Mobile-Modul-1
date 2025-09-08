5cdf8aafb9732d3764de60b5210d5cef416c995a
Faiq Rafii <rafiiwahyudi25@gmail.com>
2025-09-08 13:27:04 +0700

    up

--- lib/main.dart
+++ lib/main.dart
@@ -104,19 +104,11 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('Haloo'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            const Text('Hello World'),
+            Text("Nama Saya M. Faiq Rafii Wahyudi"),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
